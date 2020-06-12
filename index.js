8bae4ece0c5828a54463b30463182099a7c92ec2
Andy Mantell <andy.mantell@surevine.com>
2020-06-12 16:00:44 +0100

    Add some more todos

--- index.js
+++ index.js
@@ -36,3 +36,10 @@ performDiff(argv._[0]);
 
 // TODO: Make govuk-frontend version a configurable parameter
 // TODO: Make the examples fetch script actually use the cache (Currently writes to it but doesn't use it)
+// TODO: Get Jest back in properly - it's become a bit horrible without it
+// TODO: Tidy up
+// TODO: Test suite for this package + Github actions
+// TODO: Pipeline to publish package
+// TODO: Documentation
+// TODO: Investigate bundling up with https://github.com/vercel/pkg
+// TODO: Roll pull requests against govuk-react-jsx and govuk-frontend-jinja using this package

d4e50330f4356b168bb332a290cd51b8b277d455
Andy Mantell <andy.mantell@surevine.com>
2020-06-17 14:47:06 +0100

    write basic test for CLI with no args specified

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -1,5 +1,11 @@
-describe('govuk-frontend-diff CLI', () => {
-  it('works', () => {
-    expect(true).toBe(true);
+const exec = require('child_process').execFileSync;
+
+const command = process.env.GOVUK_FRONTEND_DIFF_COMMAND;
+
+describe('govuk-frontend-diff cli', () => {
+  it('outputs help message if no arguments supplied', () => {
+    expect(() => {
+      exec(command);
+    }).toThrowErrorMatchingSnapshot();
   });
 });

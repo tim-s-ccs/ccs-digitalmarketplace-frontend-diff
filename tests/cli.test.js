c0e21285b680338e7a6f3e5b46257d0c67480470
Andy Mantell <andy.mantell@surevine.com>
2020-06-19 15:49:27 +0100

    Write some additional tests

--- tests/cli.test.js
+++ tests/cli.test.js
@@ -74,22 +74,50 @@ describe('govuk-frontend-diff cli', () => {
   });
 
   it('can run a subset of the checks by excluding some', async (done) => {
+    const { exitCode, stdout, stderr } = await run(
+      `"node tests/utils/render-nunjucks.js --govuk-frontend-version=v3.7.0" --govuk-frontend-version=v3.7.0 --exclude button accordion page-template`
+    );
+    expect(stdout + stderr).toMatchSnapshot();
+    expect(exitCode).toEqual(0);
     done();
   });
 
   it('can run a subset of the checks by explicitly including some', async (done) => {
+    const { exitCode, stdout, stderr } = await run(
+      `"node tests/utils/render-nunjucks.js --govuk-frontend-version=v3.7.0" --govuk-frontend-version=v3.7.0 --include button page-template`
+    );
+    expect(stdout + stderr).toMatchSnapshot();
+    expect(exitCode).toEqual(0);
     done();
   });
 
   it('throws an error if the passed render script throws an error', async (done) => {
+    const { exitCode, stdout, stderr } = await run(
+      `"node tests/utils/script-with-error.js" --govuk-frontend-version=v3.7.0`
+    );
+    expect(stdout + stderr).toMatchSnapshot();
+    expect(exitCode).toEqual(1);
     done();
   });
 
   it('displays html diffs', async (done) => {
+    await fetchGovukFrontend('v3.0.0', {});
+
+    const { exitCode, stdout, stderr } = await run(
+      `"node tests/utils/render-nunjucks.js --govuk-frontend-version=v3.0.0" --govuk-frontend-version=v3.7.0 --include button`
+    );
+    expect(stdout + stderr).toMatchSnapshot();
+    expect(exitCode).toEqual(1);
     done();
   });
 
   it('hides html diffs when requested', async (done) => {
+    await fetchGovukFrontend('v3.0.0', {});
+    const { exitCode, stdout, stderr } = await run(
+      `"node tests/utils/render-nunjucks.js --govuk-frontend-version=v3.0.0" --govuk-frontend-version=v3.7.0 --include button --hide-diffs`
+    );
+    expect(stdout + stderr).toMatchSnapshot();
+    expect(exitCode).toEqual(1);
     done();
   });
 });

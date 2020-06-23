0c50d65a43c92b4f408414e430061e2ad21123e1
Andy Mantell <andy.mantell@surevine.com>
2020-06-23 10:39:01 +0100

    Tweak pre release checks

--- scripts/check-package.js
+++ scripts/check-package.js
@@ -6,13 +6,13 @@ const packageLockJson = require('../package-lock.json');
 const { argv } = yargs.option('tag-name');
 
 assert.strictEqual(
-  `v${packageJson.version}`,
+  `refs/tags/v${packageJson.version}`,
   argv['tag-name'],
   "Version entry in package.json doesn't match the Github tag you are trying to release"
 );
 
 assert.strictEqual(
-  `v${packageLockJson.version}`,
+  `refs/tags/v${packageLockJson.version}`,
   argv['tag-name'],
   "Version entry in package-lock.json doesn't match the Github tag you are trying to release"
 );
